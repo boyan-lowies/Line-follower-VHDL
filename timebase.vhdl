1f880ecc142614c7e914c078b8fd48de01d9d767
Boyan Lowies <lowiesboyan@gmail.com>
2023-03-09 11:52:03 +0100

    Timebase works with timebase_testbench

--- timebase.vhdl
+++ timebase.vhdl
@@ -1,25 +1,29 @@
 library ieee;
-use ieee.std_logic_1164.all;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity timebase is
     port(
         clk         :in std_logic;
         reset       :in std_logic;
-        count_out   :out std_logic_vector(0 to 19)
+        count_out   :out std_logic_vector(19 downto 0)
     );
 end entity timebase;
 
 architecture behavioral of timebase is
-	signal count, new_count		:unsigned (0 to 19);
-	
+	signal count, new_count		:unsigned (19 downto 0);
+	signal test 				:unsigned (19 downto 0);
+
 	begin
+
+		test <= (others => '1');
 		-- Register
 		process(clk)
 		begin
 			if (rising_edge(clk)) then
 				if (reset = '1') then
 					count <= (others => '0');
-				else then
+				else 
 					count <= new_count;
 				end if;
 			end if;
--- timebase_testbench.vhdl
+++ timebase_testbench.vhdl
@@ -9,22 +9,23 @@ architecture structural of testbench is
         port(
             clk         :in std_logic;
             reset       :in std_logic;
-            count_out   :out std_logic_vector(0 to 19);
+            count_out   :out std_logic_vector(19 downto 0)
         );
     end component timebase;
 
     signal clk          : std_logic;
     signal reset        : std_logic;
-    signal count_out    : unsigned (0 to 19);
+    signal count_out    : std_logic_vector(19 downto 0);
 
 begin
-    clk     <=  '1' after 0ns,
-                '0' after 10ns when clk = '1' else '1' after 10ns;
-    reset   <=  '1' after 100ns;
+    clk         <=  '1' after 0ns,
+                    '0' after 10ns when clk = '1' else '1' after 10ns;
+    reset       <=  '1' after 0ns,
+                    '0' after 60ns;
 
 P1: timebase port map(
                         clk => clk,
                         reset => reset,
-                        count_out => count_out;
-)
+                        count_out => count_out
+);
 end architecture structural;
